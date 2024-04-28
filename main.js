1221332c5b47374ee199dc828f8e6086450c8be3
Bhunesh ojha108 <149822963+Bhuneshojha@users.noreply.github.com>
2024-04-28 09:18:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+"use strict";
+//          100 days of coding challenge ( DAY 40 )
+Object.defineProperty(exports, "__esModule", { value: true });
+// Question 01  
+// --This loop logs numbers from 1 to 20
+for (var i = 1; i <= 20; i++) {
+    console.log(i);
+}
+// --Each iteration increases the value of i by 1, logging it until it reaches 20.
+// Question 02
+var count = 0;
+while (count < 5) {
+    console.log("Hello World");
+    count++;
+}
+// --Repeats logging "Hello, World!" 5 times, once per loop iteration.
+// Question 03 
+// --Defines an array of favorite movies
+var favoriteMovies = [
+    "Ram Setu",
+    "OMG 2",
+    "Samrat Prithvi Raj",
+    "Animal",
+];
+for (var _i = 0, favoriteMovies_1 = favoriteMovies; _i < favoriteMovies_1.length; _i++) {
+    var movie = favoriteMovies_1[_i];
+    console.log(movie);
+}
+// --Each movie in the array is logged to the console, one at a time.
